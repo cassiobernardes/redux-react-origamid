3a47a47529db684d9985cf74c3ca5c63c6bd9ceb
Cássio Bernardes <cassio.bernardes@compasso.com.br>
2020-11-28 18:16:29 -0300

    modal

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -8,7 +8,6 @@ import Loading from '../Helper/Loading';
 import Error from '../Helper/Error';
 
 const Feed = ({ user }) => {
-  const [modalPhoto, setModalPhoto] = React.useState(null);
   const { infinite, loading, list, error } = useSelector((state) => state.feed);
   const dispatch = useDispatch();
 
@@ -43,12 +42,11 @@ const Feed = ({ user }) => {
 
   return (
     <div>
-      {modalPhoto && (
-        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
-      )}
+      <FeedModal />
+      {list.length > 0 && <FeedPhotos />}
       {loading && <Loading />}
       {error && <Error />}
-      {list.length > 0 && <FeedPhotos setModalPhoto={setModalPhoto} />}
+
       {!infinite && !user && (
         <p
           style={{
